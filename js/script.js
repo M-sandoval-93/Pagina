f62e87b5d0381eb25f1e48a0decc1e71f1f68dd1
Mario Sandoval <mariosandoval@MacBook-Pro-de-Mario.local>
2020-11-03 03:47:42 -0300

    creación de encabezado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 "Documento de script javaScript"
+// $(document).ready(function(){
+//     $(windows).scroll(function(){
+//         if( ($this).scrollTop() > 0 ){
+//             $('header').addClass('.cabecera');
+//         } else {
+//              $('header').removeClass('.cabecera');
+//         }
+//     })
+// })
+
+
+function cambio(){
+    var scroll = document.documentElement.scrollTop;
+    if (scroll > 0){
+        console.log("realizar cambo");
+    } else {
+        console.log("revetir cambio")
+    }
+}
+
+window.addEventListener("scroll", cambio)
